b6f832588fd2929315eaba3a5f604aa44260f50e
lvingm <38157832+lvingm@users.noreply.github.com>
2021-07-03 15:10:08 +0800

    Update vgg_bow_mat.py

--- vgg_bow_mat.py
+++ vgg_bow_mat.py
@@ -27,7 +27,7 @@ class ImgModule(nn.Module):
     def __init__(self):
         super(ImgModule, self).__init__()
         self.model = models.vgg19(pretrained=True)
-        # self.model = torch.load('/home/disk1/wangshy/vgg19.pth')
+        # self.model = torch.load('./vgg19.pth')
         self.model.classifier = nn.Sequential(*list(self.model.classifier.children())[:-2])
         self.conv3 = self.model.features[:19]
         self.conv4 = self.model.features[:28]
@@ -74,7 +74,7 @@ class WikiDataset(torchdata.Dataset):
 
     def __getitem__(self, i):
         img = self.I[i]
-        img_dir = '/home/disk1/wangshy/data/wikipedia_dataset/images/' + img + '.jpg'
+        img_dir = './data/wikipedia_dataset/images/' + img + '.jpg'
         image_data = Image.open(img_dir, 'r').convert('RGB')
         image = self.transform(image_data)
         label = self.L[i]
@@ -109,7 +109,7 @@ class IaprDataset(torchdata.Dataset):
 
     def __getitem__(self, index):
         # load img
-        img_path = '/home/disk1/wangshy/data/iapr-tc12/JPEGImages/' + self.I[index]
+        img_path = './data/iapr-tc12/JPEGImages/' + self.I[index]
         img = Image.open(img_path).convert('RGB')
         if self.transform is not None:
             img = self.transform(img)
@@ -152,7 +152,7 @@ class MirDataset(torchdata.Dataset):
         self.T = text
 
     def __getitem__(self, index):
-        img_path = '/home/disk1/wangshy/data/mir-25k/JPEGImages/' + self.I[index]
+        img_path = './data/mir-25k/JPEGImages/' + self.I[index]
         img = Image.open(img_path).convert('RGB')
         if self.transform is not None:
             img = self.transform(img)
@@ -189,7 +189,7 @@ class NusDataset(torchdata.Dataset):
         self.T = txt
 
     def __getitem__(self, index):
-        img_path = '/home/disk1/wangshy/data/nuswide/JPEGImages/' + self.I[index]
+        img_path = './data/nuswide/JPEGImages/' + self.I[index]
         img = Image.open(img_path).convert('RGB')
         if self.transform is not None:    
             img = self.transform(img)
@@ -207,115 +207,6 @@ if __name__ == '__main__':
     gpu = 'cuda:3'
     device = torch.device(gpu if torch.cuda.is_available() else "cpu")
 
-    # ## mir-25k
-    # def load_data(IL_file, IT_file, batch_size, shuffle):
-    #     data_ = MirDataset(IL_file, IT_file)
-    #     loader_ = torchdata.DataLoader(
-    #         dataset=data_,
-    #         batch_size=batch_size,
-    #         shuffle=shuffle
-    #     )
-    #     return loader_
-    # tr_ILF = '/home/disk1/wangshy/data/mir-25k/docs/train_images.txt'
-    # tr_ITF = '/home/disk1/wangshy/data/mir-25k/docs/train_bows.txt'
-    # te_ILF = '/home/disk1/wangshy/data/mir-25k/docs/test_images.txt'
-    # te_ITF = '/home/disk1/wangshy/data/mir-25k/docs/test_bows.txt'
-    # db_ILF = '/home/disk1/wangshy/data/mir-25k/docs/retrieval_images.txt'
-    # db_ITF = '/home/disk1/wangshy/data/mir-25k/docs/retrieval_bows.txt'
-    # mat_file = '/home/disk1/wangshy/data/mir-25k/ITL.mat'
-    # tr_loader = load_data(tr_ILF, tr_ITF, batch_size=batch_size, shuffle=False)
-    # te_loader = load_data(te_ILF, te_ITF, batch_size=batch_size, shuffle=False)
-    # db_loader = load_data(db_ILF, db_ITF, batch_size=batch_size, shuffle=False)
-
-    # Nuswide
-    # def load_data(IL_file, batch_size, shuffle):
-    #     data_ = NusDataset(IL_file)
-    #     loader_ = torchdata.DataLoader(
-    #         dataset=data_,
-    #         batch_size=batch_size,
-    #         shuffle=shuffle
-    #     )
-    #     return loader_
-    # tr_ILF = '/home/disk1/wangshy/data/nuswide/new_train'
-    # te_ILF = '/home/disk1/wangshy/data/nuswide/new_test'
-    # db_ILF = '/home/disk1/wangshy/data/nuswide/new_retrieval'
-    # mat_file = '/home/disk1/wangshy/data/nuswide/ITL.mat'
-    #
-    # tr_loader = load_data(tr_ILF, batch_size=batch_size, shuffle=False)
-    # te_loader = load_data(te_ILF, batch_size=batch_size, shuffle=False)
-    # db_loader = load_data(db_ILF, batch_size=batch_size, shuffle=False)
-    #
-    # model = ImgModule()
-    # # torch.nn.DataParallel(model, device_ids=[1, 2, 3])
-    # model = model.to(device)
-    # model.eval()
-    #
-    # # train set
-    # tr_conv3 = []
-    # tr_conv4 = []
-    # tr_i = []
-    # tr_t = []
-    # tr_l = []
-    # for i, (imgs, text, labels) in enumerate(tr_loader):
-    #     conv3, conv4, embed = model.forward(imgs.to(device))
-    #     print(i, conv3.size(), conv4.size(), embed.size(), text.size(), labels.size())
-    #     tr_i.append(embed.data.cpu())
-    #     tr_conv3.append(conv3.data.cpu())
-    #     tr_conv4.append(conv4.data.cpu())
-    #     tr_l.append(labels.data.cpu())
-    #     tr_t.append(text.data.cpu())
-    # tr_i = np.concatenate(tr_i)
-    # tr_conv3 = np.concatenate(tr_conv3)
-    # tr_conv4 = np.concatenate(tr_conv4)
-    # tr_l = np.concatenate(tr_l)
-    # tr_t = np.concatenate(tr_t)
-    # print('tr:',tr_i.shape, tr_conv3.shape, tr_conv4.shape, tr_t.shape, tr_l.shape)
-    #
-    # # test set
-    # te_conv3 = []
-    # te_conv4 = []
-    # te_i = []
-    # te_t = []
-    # te_l = []
-    # for i, (imgs, text, labels) in enumerate(te_loader):
-    #     conv3, conv4, embed = model.forward(imgs.to(device))
-    #     te_i.append(embed.data.cpu())
-    #     te_conv3.append(conv3.data.cpu())
-    #     te_conv4.append(conv4.data.cpu())
-    #     te_l.append(labels.data.cpu())
-    #     te_t.append(text.data.cpu())
-    # te_i = np.concatenate(te_i)
-    # te_conv3 = np.concatenate(te_conv3)
-    # te_conv4 = np.concatenate(te_conv4)
-    # te_l = np.concatenate(te_l)
-    # te_t = np.concatenate(te_t)
-    # print('te:',te_i.shape, te_conv3.shape, te_conv4.shape, te_t.shape, te_l.shape)
-    #
-    # # retrieval set
-    # db_conv3 = []
-    # db_conv4 = []
-    # db_i = []
-    # db_t = []
-    # db_l = []
-    # for i, (imgs, text, labels) in enumerate(db_loader):
-    #     conv3, conv4, embed = model.forward(imgs.to(device))
-    #     db_i.append(embed.data.cpu())
-    #     db_conv3.append(conv3.data.cpu())
-    #     db_conv4.append(conv4.data.cpu())
-    #     db_l.append(labels.data.cpu())
-    #     db_t.append(text.data.cpu())
-    # db_i = np.concatenate(db_i)
-    # db_conv3 = np.concatenate(db_conv3)
-    # db_conv4 = np.concatenate(db_conv4)
-    # db_l = np.concatenate(db_l)
-    # db_t = np.concatenate(db_t)
-    # print('db:',db_i.shape, db_conv3.shape, db_conv4.shape, db_t.shape, db_l.shape)
-    #
-    # savemat(mat_file, {'tr_conv3':tr_conv3, 'tr_conv4':tr_conv4, 'tr_i':tr_i, 'tr_t':tr_t, 'tr_l':tr_l,
-    #                    'te_conv3':te_conv3, 'te_conv4':te_conv4, 'te_i':te_i, 'te_t':te_t, 'te_l':te_l,
-    #                    'db_conv3':db_conv3, 'db_conv4':db_conv4, 'db_i':db_i, 'db_t':db_t, 'db_l':db_l})
-
-
     ## wikipedia
     def load_data(IL_file, batch_size, shuffle):
         data_ = WikiDataset(IL_file)
@@ -325,11 +216,11 @@ if __name__ == '__main__':
             shuffle=shuffle
         )
         return loader_
-    tr_ILF = '/home/disk1/wangshy/data/wikipedia_dataset/trainset_txt_img_cat.list'
-    te_ILF = '/home/disk1/wangshy/data/wikipedia_dataset/testset_txt_img_cat.list'
-    tr_TF = '/home/disk1/wangshy/data/wikipedia_dataset/train_txt.mat'
-    te_TF = '/home/disk1/wangshy/data/wikipedia_dataset/test_txt.mat'
-    mat_file = '/home/disk1/wangshy/data/wikipedia_dataset/NL2ITL.mat'
+    tr_ILF = './data/wikipedia_dataset/trainset_txt_img_cat.list'
+    te_ILF = './data/wikipedia_dataset/testset_txt_img_cat.list'
+    tr_TF = './data/wikipedia_dataset/train_txt.mat'
+    te_TF = './data/wikipedia_dataset/test_txt.mat'
+    mat_file = './data/wikipedia_dataset/NL2ITL.mat'
 
     tr_loader = load_data(tr_ILF, batch_size=batch_size, shuffle=False)
     te_loader = load_data(te_ILF, batch_size=batch_size, shuffle=False)
@@ -359,7 +250,7 @@ if __name__ == '__main__':
     tr_conv4 = np.concatenate(tr_conv4)
     # tr_conv5 = np.concatenate(tr_conv5)
     tr_l = np.concatenate(tr_l)
-    tr_t = loadmat('/home/disk1/wangshy/data/wikipedia_dataset/w2v.mat')['tr_W']
+    tr_t = loadmat('./data/wikipedia_dataset/w2v.mat')['tr_W']
     print('tr:',tr_i.shape, tr_conv3.shape, tr_conv4.shape, tr_t.shape, tr_l.shape)
 
     # test set
@@ -382,7 +273,7 @@ if __name__ == '__main__':
     # te_conv5 = np.concatenate(te_conv5)
     te_l = np.concatenate(te_l)
     idxs = sorted(random.sample([i for i in range(693)], 462))
-    te_t = loadmat('/home/disk1/wangshy/data/wikipedia_dataset/w2v.mat')['te_W']
+    te_t = loadmat('./data/wikipedia_dataset/w2v.mat')['te_W']
     te_i_ = te_i[idxs]
     te_t_ = te_t[idxs]
     te_l_ = te_l[idxs]
